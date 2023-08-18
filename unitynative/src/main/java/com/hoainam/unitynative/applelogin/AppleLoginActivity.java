4316a84f367a9ca21a99aa8de01bc749d1f63451
☆ Nam ☆ <nguyenhoainam@hoppergames.io>
2023-08-18 14:13:00 +0700

    finish webview

--- unitynative/src/main/java/com/hoainam/unitynative/applelogin/AppleLoginActivity.java
+++ unitynative/src/main/java/com/hoainam/unitynative/applelogin/AppleLoginActivity.java
@@ -3,22 +3,37 @@ package com.hoainam.unitynative.applelogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 
 import com.hoainam.unitynative.R;
 
 public class AppleLoginActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apple_login);
 
-        SetupWebview(findViewById(R.id.webview_apple_login));
+        webView = findViewById(R.id.webview_apple_login);
+
+        SetupWebview();
     }
 
-    private void SetupWebview(WebView webView){
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void SetupWebview(){
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new AppleLoginWebClient());
         webView.loadUrl("https://www.google.com/");
     }
 }
