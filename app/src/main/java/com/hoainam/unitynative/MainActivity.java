fb64d13ad51ce8b1292c43f5a9919d16cc1a9f12
☆ Nam ☆ <nguyenhoainam@hoppergames.io>
2023-12-21 12:03:49 +0700

    remove ump

--- app/src/main/java/com/hoainam/unitynative/MainActivity.java
+++ app/src/main/java/com/hoainam/unitynative/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.hoainam.unitynative.admobump.AdmobUMP;
 import com.hoainam.unitynative.applelogin.AppleLogin;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn_apple_login).setOnClickListener(view -> {
-
-            AdmobUMP.ShowConsentForm(this, "9176C6C1A3DBB750F930F93CFE6434B9");
 
 //            AppleLogin.Login(this,
 //                    "com.mobirix.mbgk.web",
